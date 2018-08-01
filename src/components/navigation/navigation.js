4427c4f8a0135e8bd1119fe0ba71557d4995fc8f
AlexeyStep <lexa-chel@inbox.ru>
2018-08-01 23:23:25 +0500

    Added animation for navigation

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -3,13 +3,36 @@ import './hamburger.scss';
 
 export default class Nav {
     constructor() {
-        this.el = document.querySelector('.js-nav');
+        console.log('nav');
+        this.$el = document.querySelector('.js-nav');
         this.init();
     }
     init() {
-        if (!this.el) {
+        if (!this.$el) {
             return;
         }
+
+        this.isOpened = false;
+        this.$burger = this.$el.querySelector('.js-nav-burger');
+        console.log(this.$burger);
+        this.$burger.addEventListener('click', this.burgerClickHandler.bind(this));
         
+    }
+
+    burgerClickHandler() {
+        console.log('click');
+        this[this.isOpened ? 'close' : 'open']();
+    }
+
+    close() {
+        this.isOpened = false;
+        this.$burger.classList.remove('is-active');
+        this.$el.classList.remove('is-opened');
+    }
+
+    open() {
+        this.isOpened = true;
+        this.$burger.classList.add('is-active');
+        this.$el.classList.add('is-opened');
     }
 }
